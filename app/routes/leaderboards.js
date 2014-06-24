def8abbc1ca79ef1aa638129c3455f4528995bab
wesmangum <wesmangum24@gmail.com>
2014-06-24 13:41:15 -0500

    final commit

---
+++ app/routes/leaderboards.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var traceur = require('traceur');
+var Leaderboard = traceur.require(__dirname + '/../models/leaderboard.js');
+
+exports.get = (req, res)=>{
+	Leaderboard.findByCharacter(req.query, scores=>{
+		console.log(scores);
+		res.render('leaderboards/index', {scores: scores}, (err, html)=>{
+			res.send(html);
+		});
+	});
+};
+
+exports.submit = (req, res)=>{
+	Leaderboard.submitScore(req.body, ()=>{
+		Leaderboard.findByCharacter(req.body, scores=>{
+			res.render('leaderboards/index', {scores: scores}, (err, html)=>{
+				res.send(html);
+			});
+		});
+	});
+};
